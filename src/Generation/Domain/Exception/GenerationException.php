0d5c69d977673e81ffe964d5616708800c48787f
Oleg Patsay <smmartbiz@gmail.com>
2025-07-22 13:17:30 +0300

    FEAT: Introduce GenerationException class to handle generation process failures

---
+++ src/Generation/Domain/Exception/GenerationException.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tenqz\Ollama\Generation\Domain\Exception;
+
+/**
+ * Exception that is thrown when generation process fails.
+ */
+class GenerationException extends \Exception
+{
+}
