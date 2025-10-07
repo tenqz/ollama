7542e12443774f2b648941b4c6c9b45fb411f1e2
Oleg Patsay <smmartbiz@gmail.com>
2025-10-07 14:08:24 +0300

    FEAT: Added a new EmbeddingException to handle errors when generating embeddings.

---
+++ src/Embedding/Domain/Exception/EmbeddingException.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tenqz\Ollama\Embedding\Domain\Exception;
+
+/**
+ * Exception that is thrown when embedding generation process fails.
+ */
+class EmbeddingException extends \RuntimeException
+{
+}
